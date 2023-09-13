bf70da5e21b56c5d1d0c18b25b1eb822ae3b39c8
os-lab-konjac <wangmingyu21a@mails.ucas.ac.cn>
2023-09-13 16:31:35 +0000

    add decompress.c

--- Project1-BootLoader/tools/decompress.c
+++ Project1-BootLoader/tools/decompress.c
@@ -4,32 +4,23 @@
 #include <stdio.h>
 #include <string.h>
 
-#define BUFFER_SIZE 1000
+#define COM_KERNEL_LOC 0x52000000
+#define KERNEL 0x50201000
+#define OS_SIZE_LOC 0x502001fc
+#define BUF_LEN 0x51ffffff - 0x50200000
 
 int main(){
-    // data to be compressed
-    char data[BUFFER_SIZE] = "This is an example for DEFLATE compression and decompression!\0";
-    char compressed[BUFFER_SIZE];
-    char extracted[BUFFER_SIZE];
-    int data_len = strlen(data);
-
     // prepare environment
     deflate_set_memory_allocator((void * (*)(int))malloc, free);
-    struct libdeflate_compressor * compressor = deflate_alloc_compressor(1);
     struct libdeflate_decompressor * decompressor = deflate_alloc_decompressor();
 
-    // do compress
-    int out_nbytes = deflate_deflate_compress(compressor, data, data_len, compressed, BUFFER_SIZE);
-    printf("original: %d\n", data_len);
-    printf("compressed: %d\n", out_nbytes);
+    char *compressed = (char *)(COM_KERNEL_LOC);
+    char *extracted = (char *)(KERNEL);
+    int nbytes_kernel = *(int *)(OS_SIZE_LOC - 0x10);
 
     // do decompress
     int restore_nbytes = 0;
-    if(deflate_deflate_decompress(decompressor, compressed, out_nbytes, extracted, data_len, &restore_nbytes)){
-        printf("An error occurred during decompression.\n");
-        exit(1);
-    }
-    printf("decompressed: %d\n%s\n", restore_nbytes, extracted);
+    deflate_deflate_decompress(decompressor, compressed, nbytes_kernel, extracted, BUF_LEN, &restore_nbytes);
 
     return 0;
 }
