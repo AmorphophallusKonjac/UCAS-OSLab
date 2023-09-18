19dee4b08f9aebfd113867e2279c76fb603cbe5d
os-lab-konjac <wangmingyu21a@mails.ucas.ac.cn>
2023-09-18 07:52:55 +0000

    decompress: use global_decompress

--- Project1-BootLoader/tools/decompress.c
+++ Project1-BootLoader/tools/decompress.c
@@ -1,5 +1,7 @@
 #include "deflate/tinylibdeflate.h"
 
+extern struct libdeflate_decompressor global_decompressor;
+
 #define DECOM_KERNEL_LOC 0x54000000
 #define KERNEL 0x50201000
 #define OS_SIZE_LOC 0x502001fc
@@ -19,8 +21,7 @@ int main() {
 
     // do decompress
     int restore_nbytes = 0;
-    deflate_deflate_decompress(decompressor, compressed, nbytes_kernel, extracted,
-                               BUF_LEN, &restore_nbytes);
+    deflate_deflate_decompress(&global_decompressor, compressed, nbytes_kernel, extracted, BUF_LEN, &restore_nbytes);
 
     return 0;
 }
